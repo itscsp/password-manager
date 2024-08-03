8d1de270e8d951cc40abf96b30ab853b0efde048
Chethan S Poojary <86185190+itscsp@users.noreply.github.com>
2024-08-03 21:27:44 +0530

    Update class-user-api.php

--- includes/class-user-api.php
+++ includes/class-user-api.php
@@ -6,6 +6,8 @@ if (!defined('ABSPATH')) {
 
 class User_API
 {
+    private $master_password;
+
     public function __construct()
     {
         add_action('rest_api_init', array($this, 'register_api_routes'));
@@ -33,11 +35,6 @@ class User_API
             'callback' => array($this, 'login_user'),
         ));
 
-        register_rest_route('password-manager/v1', '/refresh-session', array(
-            'methods' => 'POST',
-            'callback' => array($this, 'refresh_session'),
-            'permission_callback' => array($this, 'handle_token_validation')
-        ));
 
         register_rest_route('password-manager/v1', '/logout', array(
             'methods' => 'POST',
@@ -106,105 +103,14 @@ class User_API
     public function handle_token_validation($request)
     {
 
-        $headers = getallheaders();
-        if (isset($headers['X-Session-Token'])) {
-            $session_token = sanitize_text_field($headers['X-Session-Token']);
-            $user_id = PM_Helper::get_user_id_from_token($session_token);
-            if ($user_id) {
-                // Retrieve the 'x-session-token' header
-                $headers = $request->get_header('X-Session-Token');
+        $user_id = PM_Helper::validate_session_token($request);
 
-                $secret_key = sanitize_text_field($request['token']);
-
-
-                // Log the session token
-                // error_log('Received session token: ' . $secret_key);
-
-                // Check if session token is missing
-                if (empty($secret_key)) {
-                    // error_log('Missing session token.');
-                    // return new WP_REST_Response(array('message' => 'Missing session token.'), 400);
-                    return false;
-                }
-
-                // Retrieve session token from request body
-                error_log('Retrieved session token from request body: ' . $secret_key);
-
-                if (empty($secret_key)) {
-                    // error_log('Missing session token in request body.');
-                    // return new WP_REST_Response(array('message' => 'Missing session token in request body.'), 400);
-                    return false;
-                }
-
-                // Check if session token format is valid
-                if (strpos($secret_key, '|') === false) {
-                    // error_log('Invalid session token format: ' . $secret_key);
-                    // return new WP_REST_Response(array('message' => 'Invalid session token format.'), 400);
-                    return false;
-                }
-
-                // Split the session token into key, iv, and encrypted data
-                list($key, $iv, $encrypted_data) = explode('|', $secret_key);
-
-                // // Log the split values
-                // error_log('Key: ' . $key);
-                // error_log('IV: ' . $iv);
-                // error_log('Encrypted Data: ' . $encrypted_data);
-
-                try {
-                    // Decrypt the master password
-                    $master_password = $this->get_decrypt_data($key, $iv, $encrypted_data);
-                    // error_log('Master Password Decrypted.' . $master_password);
-
-                    // Check if decryption failed
-                    if ($master_password === false) {
-                        // error_log('Decryption failed.');
-                        // return new WP_REST_Response(array('message' => 'Decryption failed.'), 403);
-                        return false;
-                    }
-
-                    // Retrieve the stored hashed master password
-                    $hashed_master_password = get_user_meta($user_id, 'pm_hashed_master_password', true);
-                    // error_log('Hashed master password from user meta: ' . $hashed_master_password);
-
-                    // Verify the provided master password
-                    if (!PM_Helper::verify_password($master_password, $hashed_master_password)) {
-                        // error_log('Invalid master password.');
-                        // return new WP_REST_Response(array('message' => 'Invalid master password.'), 403);
-                        return false;
-                    }
-
-                    // If all checks pass, allow the request to proceed
-                    // error_log('Token validation successful. Request authorized.');
-                    return true;
-                } catch (Exception $e) {
-                    // error_log('Error during decryption: ' . $e->getMessage());
-                    // return new WP_REST_Response(array('message' => 'Error during decryption: ' . $e->getMessage()), 500);
-                    return false;
-                }
-            } else {
-                // return new WP_REST_Response(array('message' => 'User Not valid.'), 403);
-                return false;
-            }
+        if ($user_id) {
+            $this->master_password = PM_Helper::get_master_password($request);
+            return $this->master_password !== false;
         }
+        return false;
     }
-
-
-
-
-
-    public function decrypt_data($encrypted_data, $key, $iv)
-    {
-        $decrypted_data = openssl_decrypt($encrypted_data, 'aes-256-cbc', $key, OPENSSL_RAW_DATA, $iv);
-
-        if ($decrypted_data === false) {
-            return 'Decryption failed. Encrypted data: ' . bin2hex($encrypted_data) . ', Key: ' . bin2hex($key) . ', IV: ' . bin2hex($iv);
-        }
-
-        return $decrypted_data;
-    }
-
-
 
 
     public function verify_email($request)
@@ -305,100 +211,9 @@ class User_API
         // Generate a session token
         $session_token = PM_Helper::generate_session_token($user->ID);
 
-        
-        // // Generate the encryption key from the master password and salt
-        // $encryption_key = PM_Helper::generate_encryption_key($master_password, $salt);
-        // error_log('Encryption key generated: ' . bin2hex($encryption_key)); // Debugging statement
 
-        // // Decrypt the stored secret key using the derived encryption key
-        // $decrypted_secret_key = PM_Helper::decrypt_data($encrypted_secret_key, $encryption_key);
-        // error_log('Decrypted secret key: ' . $decrypted_secret_key); // Debugging statement
-
-        // // If decryption failed, consider the login attempt as failed
-        // if ($decrypted_secret_key === false) {
-        //     error_log('Decryption failed for user ID: ' . $user->ID); // Debugging statement
-        //     return new WP_REST_Response(array('message' => 'Invalid secret key or decryption failed.'), 403);
-        // }
-        
         // Return the session token
         return new WP_REST_Response(array('message' => 'User logged in successfully.', 'token' => $session_token), 200);
-    }
-
-
-
-
-    public function refresh_session($request)
-    {
-        $headers = getallheaders();
-        if (isset($headers['X-Session-Token'])) {
-            $session_token = sanitize_text_field($headers['X-Session-Token']);
-            $user_id = PM_Helper::get_user_id_from_token($session_token);
-            if ($user_id) {
-                // Validate the session token
-                $stored_session_token = get_user_meta($user_id, 'pm_session_token', true);
-                $session_token_expiration = get_user_meta($user_id, 'pm_session_token_expiration', true);
-
-                if ($stored_session_token !== $session_token || time() > $session_token_expiration) {
-                    return new WP_REST_Response(array('message' => 'Invalid or expired session token.'), 403);
-                }
-
-                // Renew the session token expiration
-                update_user_meta($user_id, 'pm_session_token_expiration', time() + 600); // 10 minutes expiration
-
-                return new WP_REST_Response(array('message' => 'Session refreshed.', 'session_token' => $session_token), 200);
-            }
-        }
-
-        return new WP_REST_Response(array('message' => 'Unauthorized'), 401);
-    }
-
-    public function reset_master_password($request)
-    {
-        $username = sanitize_text_field($request['username']);
-        $backup_code = sanitize_text_field($request['backup_code']);
-        $new_master_password = sanitize_text_field($request['new_master_password']);
-
-        // Authenticate user by username
-        $user = get_user_by('login', $username);
-        if (!$user || is_wp_error($user)) {
-            return new WP_REST_Response(array('message' => 'Invalid username.'), 403);
-        }
-
-        // Retrieve the stored backup codes
-        $stored_backup_codes = json_decode(get_user_meta($user->ID, 'pm_backup_codes', true), true);
-
-        // Verify the backup code
-        if (!in_array($backup_code, $stored_backup_codes)) {
-            return new WP_REST_Response(array('message' => 'Invalid backup code.'), 403);
-        }
-
-        // Remove the used backup code
-        $stored_backup_codes = array_diff($stored_backup_codes, [$backup_code]);
-        update_user_meta($user->ID, 'pm_backup_codes', json_encode($stored_backup_codes));
-
-        // Hash the new master password
-        $hashed_master_password = PM_Helper::hash_password($new_master_password);
-
-        // Generate a new salt and encryption key from the new master password
-        $salt = bin2hex(random_bytes(16));
-        $encryption_key = PM_Helper::generate_encryption_key($new_master_password, $salt);
-
-        // Retrieve the current encrypted secret key
-        $encrypted_secret_key = get_user_meta($user->ID, 'pm_encrypted_secret_key', true);
-
-        // Decrypt the secret key using the old encryption key
-        $old_encryption_key = PM_Helper::generate_encryption_key($new_master_password, $salt);
-        $secret_key = PM_Helper::decrypt_data($encrypted_secret_key, $old_encryption_key);
-
-        // Encrypt the secret key using the new encryption key
-        $new_encrypted_secret_key = PM_Helper::encrypt_data($secret_key, $encryption_key);
-
-        // Store the new hashed master password, encrypted secret key, and salt in user meta
-        update_user_meta($user->ID, 'pm_hashed_master_password', $hashed_master_password);
-        update_user_meta($user->ID, 'pm_encrypted_secret_key', $new_encrypted_secret_key);
-        update_user_meta($user->ID, 'pm_salt', $salt);
-
-        return new WP_REST_Response(array('message' => 'Master password reset successfully.'), 200);
     }
 
 
