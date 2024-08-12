be232ec627b1216ed962cde2846484355e145d03
itscsp <chethanspoojary1@gmail.com>
2024-08-12 21:32:39 +0530

    Building site layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="fixed bottom-0 left-0 w-full text-center py-4  text-white font-inter">
+    <footer className="text-center py-4  text-white font-inter">
       Designed And Built By <Link target="_blank" to="https://chethanspoojary.com/" className="text-red">Chethan S Poojary</Link>
     </footer>
   );
--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,21 +1,31 @@
+import { Link } from "react-router-dom"
+import logo from "../assets/onepass_logo.svg"
+
 import Button from "../components/Button"
+import Footer from "../components/Footer"
 import Header from "../components/Header"
 
 const NotFoundPage = () => {
   return (
-    <div className="flex flex-col justify-center">
-        <Header />
+    <>
+      <Header />
+      <main className="mainLayout">
+        <Link to="/" className='flex justify-center items-center mb-8'>
+          <img className='w-[125px]' src={logo} alt="" />
+        </Link>
         <h1 className="text-center mb-4 text-3xl mb-6">404 | Page Not Found</h1>
         <div className="mx-auto">
 
-        <Button 
-        text="Back To Home"
-        url="/"
-        variation="primary"
-        size="inline"
-        />
+          <Button
+            text="Back To Home"
+            url="/"
+            variation="primary"
+            size="inline"
+          />
         </div>
-    </div>
+      </main>
+      <Footer />
+    </>
   )
 }
 
