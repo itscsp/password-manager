cae04600cb71bc8e28dfb41902be68d97fb7273b
itscsp <chethanspoojary1@gmail.com>
2024-08-26 01:49:55 +0530

    Small footer design fix

--- src/_components/Footer.tsx
+++ src/_components/Footer.tsx
@@ -4,8 +4,8 @@ export {Footer}
 
 function Footer() {
     return (
-        <footer className="w-full text-center py-4  text-white">
-        Designed And Built By <Link target="_blank" to="https://chethanspoojary.com/" className="text-opred">Chethan S Poojary</Link>
+        <footer className="w-full text-center py-4  text-white text-[12px]">
+        Designed and Developed by <Link target="_blank" to="https://chethanspoojary.com/" className="text-opred">Chethan S Poojary</Link>
       </footer>
     )
 }
